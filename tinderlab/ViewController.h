8a2e8ad930ea5f011c48e400e43121d260791a21
Amit Agarwal <lifeamit@gmail.com>
2015-09-18 12:41:24 -0700

    milestone 1

---
+++ tinderlab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tinderlab
+//
+//  Created by Amit Agarwal on 9/18/15.
+//  Copyright © 2015 Amit Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tinderlab/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tinderlab
+//
+//  Created by Amit Agarwal on 9/18/15.
+//  Copyright © 2015 Amit Agarwal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
